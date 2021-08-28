3c60b34fda495536bc77202f8ed71738ce0482f7
Joyce Ye <46504694+yejoyce8@users.noreply.github.com>
2021-08-28 15:48:13 -0400

    added packages for react navigation

--- src/components/App.js
+++ src/components/App.js
@@ -2,40 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 // import styles from './stylestest.js'
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-// function HomeScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Home!</Text>
-//     </View>
-//   );
-// }
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
 
-// function SettingsScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Settings!</Text>
-//     </View>
-//   );
-// }
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
 
-// const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
-// function MyTabs() {
-//   return (
-//     <Tab.Navigator>
-//       <Tab.Screen name="Home" component={HomeScreen} />
-//       <Tab.Screen name="Settings" component={SettingsScreen} />
-//     </Tab.Navigator>
-//   );
-// }
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     //wrap everything around the navigationcontainer
-    // <NavigationContainer>
+    <NavigationContainer>
       <View style={styles.container}>
         <Text style={[{color: 'white'},{backgroundColor: 'black'}]}>ONE</Text>
         <Text style={{color: 'blue'}}>TWO</Text>
@@ -44,9 +44,9 @@ export default function App() {
         <StatusBar style="auto" />
       </View>
 
-      // <MyTabs />
+      <MyTabs />
       
-    // </NavigationContainer>
+    </NavigationContainer>
     
     
   );
