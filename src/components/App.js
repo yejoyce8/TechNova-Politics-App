955c6fa821ef75986891a8893d860b72a767a835
Joyce Ye <46504694+yejoyce8@users.noreply.github.com>
2021-08-28 15:49:36 -0400

    packages + nav bar, prototype pages work in progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,74 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-// import styles from './stylestest.js'
+import * as React from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {useState, useEffect} from 'react'
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
+//import the component pages
+import HomeScreen from './screens/homepagescreen'
+import TwitterLiveFeedScreen from './screens/twitterlivefeedscreen'
+import GeneralInfoScreen from './screens/generalinfoscreen'
+import LocationServiceScreen from './screens/locationservicescreen'
+import MPDatabaseScreen from './screens/mpdatabasescreen'
 
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+
+
+
 
 const Tab = createBottomTabNavigator();
 
+//Tabs menu 
 function MyTabs() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Screen name="Twitter" component={TwitterLiveFeedScreen} />
+      <Tab.Screen name="Elections" component={GeneralInfoScreen} />
+      <Tab.Screen name="Local" component={LocationServiceScreen} />
+      <Tab.Screen name="MPs" component={MPDatabaseScreen} />
     </Tab.Navigator>
   );
 }
 
-export default function App() {
-  return (
-    //wrap everything around the navigationcontainer
-    <NavigationContainer>
-      <View style={styles.container}>
-        <Text style={[{color: 'white'},{backgroundColor: 'black'}]}>ONE</Text>
-        <Text style={{color: 'blue'}}>TWO</Text>
-        <Text style={{color: 'blue'}}>THREE</Text>
-        
-        <StatusBar style="auto" />
-      </View>
 
+
+
+
+export default function App() {
+  
+  return (
+    <NavigationContainer>
+      
       <MyTabs />
       
     </NavigationContainer>
-    
-    
   );
 }
 
 
-
-
-
-//styles
 const styles = StyleSheet.create({
   container: {
-    flex: 10,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-    marginTop: 80,
-    paddingHorizontal: 30
-    
-
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
   },
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  buttonStyle: {
+    marginRight:40,
+    marginLeft:40,
+    marginTop:10,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'#1E6738',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  baseText: {
+    fontFamily: "Cochin"
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
 });
+
+
 
 
 
